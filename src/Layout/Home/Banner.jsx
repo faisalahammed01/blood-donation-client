da414a9622c6bbac5674b021adbff7daeb700006
/Faisalahammed003 <faisalahammed5035@gmail.com>
2025-01-14 02:16:09 +0600

    banner-completde

---
+++ src/Layout/Home/Banner.jsx
@@ -0,0 +1,33 @@
+const Banner = () => {
+  return (
+    <section className="relative">
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co.com/K2HfhjL/Picsart-25-01-14-00-43-03-934-1.jpg)",
+        }}
+      >
+        <div className=" text-center">
+          <div className="">
+            <h1 className="mb-5 text-4xl font-bold mr-24">
+              <span className="text-white">Blood</span>{" "}
+              <span className="text-[#A72316]">Donation</span>
+            </h1>
+            <p className="mb-5 ml-36 text-[#A72316]">
+              Save lives, spread hope—donate blood today
+            </p>
+          </div>
+        </div>
+      </div>
+      <button className="absolute bottom-44 right-56 btn text-white bg-red-700 pr-20 pl-20 hover:bg-red-900">
+        Join as a donor
+      </button>
+      <button className="absolute bottom-24 right-56 btn  text-white bg-red-700 pr-20 pl-20 hover:bg-red-900">
+        Search Donors
+      </button>
+    </section>
+  );
+};
+
+export default Banner;
--- src/Layout/Outlet/Footer.jsx
+++ src/Layout/Outlet/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer bg-[#8CBDCE] text-neutral-content p-10 border-y-2 border-black">
+    <footer className="footer bg-red-900 text-neutral-content p-10 border-y-2 border-black">
       <aside>
         <div>
           <img
@@ -9,7 +9,7 @@ const Footer = () => {
             alt=""
           />
         </div>
-        <p className="text-black text-xl">Saving lives, one drop at a time.</p>
+        <p className="text-white text-xl">Saving lives, one drop at a time.</p>
       </aside>
       <nav>
         <h6 className="footer-title text-xl opacity-90 font-bold text-white">
@@ -33,7 +33,7 @@ const Footer = () => {
               width="24"
               height="24"
               viewBox="0 0 24 24"
-              className="fill-current text-red-500"
+              className="fill-current  text-red-500"
             >
               <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
             </svg>
