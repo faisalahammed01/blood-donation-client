9449de070f6febc874445aecedbdf6746628d59e
/Faisalahammed003 <faisalahammed5035@gmail.com>
2025-01-16 04:56:20 +0600

    register-page-completed

--- src/LoginAndRegister/Register.jsx
+++ src/LoginAndRegister/Register.jsx
@@ -1,6 +1,56 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
+// import useAxiosPublic from "../Share/axiosPublic";
+
+// const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+// const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
 const Register = () => {
+  const [selectedUpazila, setSelectedUpazila] = useState("");
+  const [selectedDistricts, setSelectedDistricts] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const password = watch("password");
+
+  const { data: upazilas = [], isLoading } = useQuery({
+    queryKey: ["Upazilas"],
+    queryFn: async () => {
+      const res = await axios.get("/upazilas.json");
+      return res.data;
+    },
+  });
+  const { data: districts = [] } = useQuery({
+    queryKey: ["districts"],
+    queryFn: async () => {
+      const res = await axios.get("/districts.json");
+      return res.data;
+    },
+  });
+  const onSubmit = (data) => {
+    console.log("Form Data:", data);
+    alert("Registration Successful!");
+  };
+
+  // const imageFile = { image: data.image[0] }
+  // const res = await useAxiosPublic.post(image_hosting_api, imageFile, {
+  //     headers: {
+  //         'content-type': 'multipart/form-data'
+  //     }
+  // });
+
+  if (isLoading)
+    return <span className="loading loading-spinner loading-lg"></span>;
+
   return (
     <div className="lg:w-3/4 mx-auto">
       <div className="text-center p-10">
@@ -11,7 +61,7 @@ const Register = () => {
         />
       </div>
       <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
-        <form className="card-body">
+        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
           {/* form first row */}
           <div className="flex flex-col lg:flex-row gap-5">
             <div className="form-control flex-1">
@@ -21,7 +71,7 @@ const Register = () => {
               <input
                 type="text"
                 name=""
-                placeholder="user details"
+                placeholder="Name"
                 className="input input-bordered"
                 required
               />
@@ -33,7 +83,7 @@ const Register = () => {
               <input
                 type="email"
                 name="Category"
-                placeholder="e.g., pets, documents, gadgets"
+                placeholder="Email"
                 className="input input-bordered"
                 required
               />
@@ -48,7 +98,7 @@ const Register = () => {
               <input
                 type="file"
                 name="Thumbnail"
-                placeholder="Image url"
+                placeholder="Avatar"
                 className="input input-bordered"
                 required
               />
@@ -57,13 +107,24 @@ const Register = () => {
               <label className="label">
                 <span className="label-text">Blood Group</span>
               </label>
-              <input
-                type="text"
-                name="Title"
-                placeholder="Title"
-                className="input input-bordered"
+              <select
+                defaultValue="default"
+                name="Blood"
                 required
-              />
+                className="select select-bordered w-full "
+              >
+                <option disabled value="default">
+                  Select Blood Group
+                </option>
+                <option value="A+">A+</option>
+                <option value="A-">A-</option>
+                <option value="B+">B+</option>
+                <option value="B-">B-</option>
+                <option value="AB+">AB+</option>
+                <option value="AB-">AB-</option>
+                <option value="O+">O+</option>
+                <option value="O-">O-</option>
+              </select>
             </div>
           </div>
           {/* form third row */}
@@ -72,25 +133,44 @@ const Register = () => {
               <label className="label">
                 <span className="label-text">District</span>
               </label>
-              <input
-                type="text"
-                name="description"
-                placeholder="Description"
-                className="input input-bordered"
+              <select
+                className="select select-bordered"
+                name="Upazila"
+                value={selectedDistricts}
+                onChange={(e) => setSelectedDistricts(e.target.value)}
                 required
-              />
+              >
+                <option value="" disabled>
+                  Select District
+                </option>
+                {districts.map((district) => (
+                  <option key={district.id} value={district.id}>
+                    {district.name}
+                  </option>
+                ))}
+              </select>
             </div>
+            {/*------------------------------//-------------------------------------------- */}
             <div className="form-control flex-1">
               <label className="label">
                 <span className="label-text">Upazila</span>
               </label>
-              <input
-                type="text"
-                name="location"
-                placeholder="Location where the item was lost or found"
-                className="input input-bordered"
+              <select
+                className="select select-bordered"
+                name="Upazila"
+                value={selectedUpazila}
+                onChange={(e) => setSelectedUpazila(e.target.value)}
                 required
-              />
+              >
+                <option value="" disabled>
+                  Select Upazila
+                </option>
+                {upazilas.map((upazila) => (
+                  <option key={upazila.id} value={upazila.id}>
+                    {upazila.name}
+                  </option>
+                ))}
+              </select>
             </div>
           </div>
           {/* form four row */}
@@ -103,6 +183,7 @@ const Register = () => {
                 type="password"
                 name="Contact"
                 placeholder="Password"
+                {...register("password", { required: "Password is required" })}
                 className="input input-bordered"
                 required
               />
@@ -116,11 +197,20 @@ const Register = () => {
                 name="Contact"
                 placeholder="Confirm_password"
                 className="input input-bordered"
+                {...register("confirmPassword", {
+                  required: "Confirm Password is required",
+                  validate: (value) =>
+                    value === password || "Passwords do not match",
+                })}
                 required
               />
             </div>
           </div>
-
+          {errors.confirmPassword && (
+            <p className="text-red-600 mt-2">
+              {errors.confirmPassword.message}
+            </p>
+          )}
           <div className="form-control mt-6">
             <button className="btn btn-outline text-white font-bold bg-red-800 hover:bg-red-950">
               Register
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import LayOut from "./Layout/Outlet/LayOut";
 import Login from "./LoginAndRegister/Login";
 import Register from "./LoginAndRegister/Register";
 import Blog from "./Pages/Blog";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -36,7 +38,9 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </QueryClientProvider>
 );
